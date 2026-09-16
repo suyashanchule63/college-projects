9100229b9251adf6a4557f0df3f65b144a225bc7
suyashanchule63 <suyashanchule252@gmail.com>
2026-09-16 21:34:04 +0530

    Create third.c

---
+++ third.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int num = 7;
+    if (num % 2 == 0) {
+        printf("%d is Even\n", num);
+    } else {
+        printf("%d is Odd\n", num);
+    }
+    return 0;
+}
