8d5ba369660afe70852a9a6c613940c8e5d5b82c
suyashanchule63 <suyashanchule252@gmail.com>
2026-09-17 22:01:07 +0530

    Create day2_second.c

---
+++ day2_second.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num = -10;
+
+    if (num >= 0) {
+        printf("%d is positive\n", num);
+    } else {
+        printf("%d is negative\n", num);
+    }
+
+    return 0;
+}
