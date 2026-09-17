5e1b9cf9a0ee81488abfa75f448f87155120ed81
suyashanchule63 <suyashanchule252@gmail.com>
2026-09-17 21:57:38 +0530

    Create day2_first.c

---
+++ day2_first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 15, num2 = 20;
+
+    if (num1 > num2) {
+        printf("%d is larger\n", num1);
+    } else {
+        printf("%d is larger\n", num2);
+    }
+
+    return 0;
+}
