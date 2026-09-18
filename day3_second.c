8cbd1a229adac2035f8f464d42aaad3e220ad7f8
suyashanchule63 <suyashanchule252@gmail.com>
2026-09-18 21:17:35 +0530

    Create day3_second.c

---
+++ day3_second.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    float celsius = 25.0;
+    float fahrenheit = (celsius * 9 / 5) + 32;
+    printf("25C in Fahrenheit is: %.1f\n", fahrenheit);
+    return 0;
+}
