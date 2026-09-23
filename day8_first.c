641bf4dabcc91904b85d2064bfdd13395f41d98d
suyashanchule63 <suyashanchule252@gmail.com>
2026-09-23 22:08:43 +0530

    Create day8_first.c

---
+++ day8_first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a = 15, b = 8;
+    if (a < b) {
+        printf("%d is smaller\n", a);
+    } else {
+        printf("%d is smaller\n", b);
+    }
+    return 0;
+}
