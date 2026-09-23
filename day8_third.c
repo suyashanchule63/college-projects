b87923eeae79351dc2db6d75ce0bb897826b1987
suyashanchule63 <suyashanchule252@gmail.com>
2026-09-23 22:11:05 +0530

    Create day8_third.c

---
+++ day8_third.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int age = 19;
+    if (age >= 18) {
+        printf("Eligible to vote\n");
+    } else {
+        printf("Not eligible to vote\n");
+    }
+    return 0;
+}
