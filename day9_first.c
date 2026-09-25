f98dd366e5917e0968d92dc65349ff9941d690dc
suyashanchule63 <suyashanchule252@gmail.com>
2026-09-25 23:28:42 +0530

    Create day9_first.c

---
+++ day9_first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int x = 45, y = 30;
+    if (x > y) {
+        printf("%d is greater\n", x);
+    } else {
+        printf("%d is greater\n", y);
+    }
+    return 0;
+}
