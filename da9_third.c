eda704643a88ca225f2b82fba11c883eb89d6648
suyashanchule63 <suyashanchule252@gmail.com>
2026-09-25 23:29:52 +0530

    Create da9_third.c

---
+++ da9_third.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int amount = 1200;
+    if (amount > 1000) {
+        printf("Eligible for discount!\n");
+    } else {
+        printf("No discount.\n");
+    }
+    return 0;
+}
