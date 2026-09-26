da16c45c3737f1cb6a380e10c6b01ef136e77206
suyashanchule63 <suyashanchule252@gmail.com>
2026-09-26 23:05:35 +0530

    Create day10_third.c

---
+++ day10_third.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int speed = 65;
+    if (speed > 60) {
+        printf("Over speed limit!\n");
+    } else {
+        printf("Normal speed\n");
+    }
+    return 0;
+}
