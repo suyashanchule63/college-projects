9b5c740b7958681c2937aa2eedf530fe58c728bb
suyashanchule63 <suyashanchule252@gmail.com>
2026-09-26 23:03:09 +0530

    Create day10_first.c

---
+++ day10_first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int num = 0;
+    if (num == 0) {
+        printf("The number is zero\n");
+    } else {
+        printf("The number is not zero\n");
+    }
+    return 0;
+}
